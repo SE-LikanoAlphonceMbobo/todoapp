bc97e2464db6e208e25d49259359dedca9c8397e
Likano Mbobo <likanomoney@gmail.com>
2025-01-22 15:16:11 +0200

    Added a button alert

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi Likano!!</Text>
+      <Text>Welcome to React Native!!</Text>
+      <Button title="Alert" onPress={() => alert("Hi, Likano")} />
       <StatusBar style="auto" />
     </View>
   );
